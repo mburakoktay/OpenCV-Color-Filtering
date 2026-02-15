3ef5152b914911e5b639259bf86021ed55c0d826
mburakoktay <burakk1085@gmail.com>
2026-02-15 20:14:52 +0300

    Update Color_Filtering.py

--- Color_Filtering.py
+++ Color_Filtering.py
@@ -2,33 +2,33 @@ import cv2 as cv
 import numpy as np
 
 
-# Boş bir fonksiyon (Trackbar için gerekli)
+# for tranckbar
 def nothing(x):
     pass
 
 
-# 1. Kontrol Penceresi ve Trackbarlar
+# Trackbars
 cv.namedWindow("Trackbar")
 
 # Senin değişken isimlerini daha net hale getirdik (azMavi -> minMavi)
 cv.createTrackbar("minMavi", "Trackbar", 0, 255, nothing)
-cv.createTrackbar("maxMavi", "Trackbar", 255, 255, nothing)
+cv.createTrackbar("maxMavi", "Trackbar", 0, 255, nothing)
 cv.createTrackbar("minYesil", "Trackbar", 0, 255, nothing)
-cv.createTrackbar("maxYesil", "Trackbar", 255, 255, nothing)
+cv.createTrackbar("maxYesil", "Trackbar", 0, 255, nothing)
 cv.createTrackbar("minKirmizi", "Trackbar", 0, 255, nothing)
-cv.createTrackbar("maxKirmizi", "Trackbar", 255, 255, nothing)
+cv.createTrackbar("maxKirmizi", "Trackbar", 0, 255, nothing)
 
-# Video dosyasını oku
+# Read video
 takip = cv.VideoCapture("takip.mp4")
 
 while True:
     kontrol, frame = takip.read()
 
-    # Video biterse döngüyü kır (Hata almamak için)
+    
     if not kontrol:
         break
 
-    # Trackbar değerlerini oku
+    # Read trackbar's value
     minM = cv.getTrackbarPos("minMavi", "Trackbar")
     maxM = cv.getTrackbarPos("maxMavi", "Trackbar")
     minY = cv.getTrackbarPos("minYesil", "Trackbar")
@@ -36,22 +36,22 @@ while True:
     minK = cv.getTrackbarPos("minKirmizi", "Trackbar")
     maxK = cv.getTrackbarPos("maxKirmizi", "Trackbar")
 
-    # Renk aralıklarını tanımla (BGR sırasıyla)
+    
     alt_sinir = np.array([minM, minY, minK])
     ust_sinir = np.array([maxM, maxY, maxK])
 
-    # Maskeleme ve Sonuç
+    # Mask 
     maske = cv.inRange(frame, alt_sinir, ust_sinir)
     son_hal = cv.bitwise_and(frame, frame, mask=maske)
 
-    # Görüntüleri göster
+    # Show videos
     cv.imshow("Orijinal Video", frame)
     cv.imshow("Maske (Siyah-Beyaz)", maske)
     cv.imshow("Filtrelenmiş Sonuc", son_hal)
 
-    # 'q' tuşu ile çıkış yap
+    # Exit with "q"
     if cv.waitKey(20) & 0xFF == ord("q"):
         break
 
-takip.release()
-cv.destroyAllWindows()
+
+
